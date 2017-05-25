261fb9af9622c0c29397ec01e415d0e010d0be92
lear <641187432@qq.com>
2017-05-25 22:47:57 +0800

    绳子动画

--- assets/script/view/gamemine/GameMineScene.js
+++ assets/script/view/gamemine/GameMineScene.js
@@ -2,6 +2,7 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+        canvas: cc.Node
     },
 
     // use this for initialization
@@ -10,11 +11,44 @@ cc.Class({
     },
 
     init () {
+        this.isDropPawing = false //抓取的状态
+        this.paw = this.canvas.getChildByName("Paw").getComponent("PawCtrl");
+        //this.miner = this.canvas.getChildByName("Miner").getComponent("Miner");
+
+        this.canvas.on(cc.Node.EventType.TOUCH_START, event => {
+            var touches = event.getTouches();
+            var touchLoc = touches[0].getLocation();
+            //console.log("--------------touch start-----------------")
+            this.dropPaw();
+
+        }, this.node);
         
+        this.canvas.on(cc.Node.EventType.TOUCH_MOVE, function (event) {
+            var touches = event.getTouches();
+            var touchLoc = touches[0].getLocation();
+            console.log("--------------touch move-----------------")
+
+        }, this.node);
+
+        this.canvas.on(cc.Node.EventType.TOUCH_END, function (event) {
+            console.log("--------------touch end-----------------")
+
+        }, this.node);
+
     },
 
     // called every frame
     update: function (dt) {
         
     },
+
+    // 放下爪子
+    dropPaw: function () {
+        if (this.paw.curState == this.paw.state.AWAIT) {
+            //this.isDropPawing = true;
+            this.paw.drop();
+        }
+    },
+
+
 });
--- assets/script/view/gamemine/PawCtrl.js
+++ assets/script/view/gamemine/PawCtrl.js
@@ -2,30 +2,146 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        rotateSpeed: 0
+        rotateSpeed: 10,
+        rotateMax: 70,
+        dropSpeed: 100,
+        raiseSpeed: 100,
     },
 
     // use this for initialization
     onLoad: function () {
         this.init();
     },
-    
-    init () {
-        this.rotateSpeed = 10;
-        this.await()
+
+    // called every frame
+    update: function (dt) {
+
     },
 
-    
+    //动画更新后执行
+    lateUpdate: function (dt) {
+        var curPawPos = this.node.getPosition();
+        var length = cc.pDistance(curPawPos, this.orginPos);
+        this.setLineLength(length);
+    },
+
+    init () {
+        this.state = {
+            AWAIT : 1, //等待状态
+            DROP : 2, //放下爪子状态
+            RAISE : 3, //提起爪子
+        };
+        this.curState = this.state.AWAIT; //当前爪子状态
+        this.orginPos = this.node.getPosition();
+
+        this.speedFix = 100; //速度修正系数
+        //this.dropSpeed = 100; //爪子下抓的速度
+        //this.raiseSpeed = 100; //爪子提起的速度
+        this.line =  this.node.getChildByName("Line");
+        this.await();
+    },
+
+    changeState (state) {
+        if (state != this.curState) {
+            this.curState = state
+        }
+    },
+
     await () {
         //待机旋转  
-         var seq = cc.repeatForever(
-             cc.sequence(
-                 cc.rotateTo(2, -70), 
-                 cc.rotateTo(2, 70)
-             ));
-        this.node.runAction(seq)
+        if (this.actionRaise) {
+            this.node.stopAction(this.actionRaise);
+            this.actionRaise = null;
+        }
+        this.changeState(this.state.AWAIT);
+        this.actionAwait = cc.repeatForever(
+            cc.sequence(
+                cc.rotateTo(this.speedFix/this.rotateSpeed, -1*this.rotateMax), 
+                cc.rotateTo(this.speedFix/this.rotateSpeed, this.rotateMax)
+            ));
+        this.node.runAction(this.actionAwait);
+    },
+
+    drop () {
+        //放下爪子
+        if (this.actionAwait) {
+            this.node.stopAction(this.actionAwait);
+            this.actionAwait = null;
+        }
+        this.changeState(this.state.DROP);
+        var ratete = this.node.getRotation();
+        var length = this.getLength(ratete);
+        var aimPos = this.getAimPoint(ratete);
+
+        this.actionDrop = cc.sequence(
+            cc.moveTo(this.speedFix/this.dropSpeed, aimPos), 
+            cc.callFunc(() => {
+                this.raise();
+            }, this)
+        )
+        this.node.runAction(this.actionDrop);
+    },
+
+    raise () {
+        //提起爪子
+        if (this.actionDrop) {
+            this.node.stopAction(this.actionDrop);
+            this.actionDrop = null;
+        }
+        this.changeState(this.state.RAISE);
+        this.actionRaise = cc.sequence(
+            cc.moveTo(this.speedFix/this.raiseSpeed, this.orginPos),
+            cc.callFunc(() => {
+                this.await();
+            }, this)
+        )
+        this.node.runAction(this.actionRaise);
+    },
+
+    //获取爪子放下的距离
+    getLength (ratete) {
+        var height = 568; //高
+        var weight = 1134/2; //宽
+        var absRatete = Math.abs(ratete);
+        var length = 0;
+        if (absRatete < 45) {
+            length = height/Math.cos(Math.abs(ratete * Math.PI / 180));
+        }else{
+            length = weight/Math.sin(Math.abs(ratete * Math.PI / 180));
+        }
+        return length
+    },
+
+    //获取爪子预计到达的目标点
+    getAimPoint (ratete) {
+        var nodePos = this.node.getPosition();
+        var x = nodePos.x;
+        var y = nodePos.y;
+        var dir = ratete/Math.abs(ratete);
+
+        var height = 550; //高
+        var weight = 1334/2; //宽
+        var absRatete = Math.abs(ratete);
+        var length = 0;
+        if (absRatete < 45) {
+            length = height * Math.tan(Math.abs(ratete * Math.PI / 180));
+            x = x - dir*length;
+            y = y - height;
+        }else{
+            length = weight/Math.tan(Math.abs(ratete * Math.PI / 180));
+            x = x - dir*weight;
+            y = y - length;
+        }
+        var pos = cc.p(x, y);
+        return pos
     },
 
 
+ //-----------------------------------Line---------------------------------
 
+    //设置绳子长度
+    setLineLength (length) {
+        this.line.height = length + 22;
+    }
+ 
 });
